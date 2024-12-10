23e15b924e92411ef73bdce963d8fcee51ec8384
Hamid <aleehamid1024@gmail.com>
2024-12-11 00:15:05 +0500

    adds jwt awth

--- src/main/java/net/oceandepth/journalApp/config/SpringSecurity.java
+++ src/main/java/net/oceandepth/journalApp/config/SpringSecurity.java
@@ -1,38 +1,46 @@
 package net.oceandepth.journalApp.config;
 
+import net.oceandepth.journalApp.filter.JwtFilter;
 import net.oceandepth.journalApp.service.UserDetailsServiceImp;
+import net.oceandepth.journalApp.utils.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-@Profile("dev")
+//@Profile("dev")
 public class SpringSecurity {
 
     UserDetailsServiceImp userDetailsServiceImp;
+    JwtFilter jwtFilter;
     @Autowired
-    public SpringSecurity(UserDetailsServiceImp userDetailsServiceImp) {
+    public SpringSecurity( UserDetailsServiceImp userDetailsServiceImp,
+                           JwtFilter jwtFilter ) {
         this.userDetailsServiceImp = userDetailsServiceImp;
+        this.jwtFilter = jwtFilter;
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, JwtUtil jwtUtil) throws Exception {
         return httpSecurity.authorizeHttpRequests(request -> request
                         .requestMatchers("/public/**").permitAll()
                         .requestMatchers("/journal/**", "/user/**").authenticated()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults())
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .csrf(AbstractHttpConfigurer::disable)
                 .build();
     }
@@ -45,5 +53,10 @@ public class SpringSecurity {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration auth) throws Exception {
+        return auth.getAuthenticationManager();
     }
 }
--- src/main/java/net/oceandepth/journalApp/controller/PublicController.java
+++ src/main/java/net/oceandepth/journalApp/controller/PublicController.java
@@ -1,20 +1,37 @@
 package net.oceandepth.journalApp.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import net.oceandepth.journalApp.entity.User;
+import net.oceandepth.journalApp.service.UserDetailsServiceImp;
 import net.oceandepth.journalApp.service.UserService;
+import net.oceandepth.journalApp.utils.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Slf4j
 @RequestMapping("/public")
 public class PublicController {
 
     UserService userService;
+    AuthenticationManager authenticationManager;
+    UserDetailsServiceImp userDetailsService;
+    JwtUtil jwtUtil;
+
     @Autowired
-    public PublicController(UserService userService) {
+    public PublicController( UserService userService,
+                             AuthenticationManager authenticationManager,
+                             UserDetailsServiceImp userDetailsService,
+                             JwtUtil jwtUtil ) {
         this.userService = userService;
+        this.authenticationManager = authenticationManager;
+        this.userDetailsService = userDetailsService;
+        this.jwtUtil = jwtUtil;
     }
 
     @GetMapping("check")
@@ -22,13 +39,27 @@ public class PublicController {
         return "ok!";
     }
 
-    @PostMapping("/create-user")
-    public ResponseEntity<User> createEntry(@RequestBody User userEntry) {
+    @PostMapping("/sign-up")
+    public ResponseEntity<User> signup(@RequestBody User userEntry) {
         try {
             userService.saveNewUser(userEntry);
             return new ResponseEntity<>(userEntry, HttpStatus.CREATED);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/log-in")
+    public ResponseEntity<String> login(@RequestBody User user) {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                    user.getUserName(), user.getPassword()));
+            UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUserName());
+            String jwt = jwtUtil.generateToken(userDetails.getUsername());
+            return new ResponseEntity<>(jwt, HttpStatus.OK);
+        } catch (Exception e) {
+            log.error("Occurred error while generating Authentication-Token", e);
+            return new ResponseEntity<>("Incorrect username or password ", HttpStatus.BAD_REQUEST);
         }
     }
 }
